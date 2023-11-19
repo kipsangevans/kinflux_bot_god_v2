e35f2ee2768e8171ac8650a2a18dd9ea3f842cbb
evanso321 <115561243+kipsangevans@users.noreply.github.com>
2023-11-19 06:57:18 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,14 +7,14 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['254741657564', 'Zneimarh_ke', true],
-  ['+254110377776', 'kinflux digital', true], 
+  ['254799695390', 'Zneimarh_ke', true],
+  ['+254799695390', 'kinflux digital', true], 
   ['556199740980'] 
 ] //Number of owners
 
-global.mods = ['13374230362','254110377776','556199740980'] 
-global.prems = ['22589747147', '254110377776', '556199740980']
-global.allowed = ['556199740980', '254110377776', '13374230362']
+global.mods = ['13374230362','254799695390','556199740980'] 
+global.prems = ['22589747147', '254799695390', '556199740980']
+global.allowed = ['556199740980', '254799695390', '13374230362']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
